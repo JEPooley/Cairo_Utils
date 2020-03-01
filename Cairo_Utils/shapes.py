ea750a723c37cdec15d1147921180b691ec99a98
Josh <joshpooley93@gmail.com>
2020-03-01 19:02:45 +0000

    add draw spheres

--- Cairo_Utils/shapes.py
+++ Cairo_Utils/shapes.py
@@ -23,8 +23,9 @@ class Shapes:
 
     @classmethod
     def draw_circle_fill_shade(cls, ctx, x, y,
-                               radius, shift, rgba):
-        cls.draw_circle_fill(ctx, x - shift, y - shift, radius, (0, 0, 0, 0.1))
+                               radius, shift, rgba, rotate=0.0):
+        x_shift, y_shift = shift * math.cos(rotate), shift * math.sin(rotate)
+        cls.draw_circle_fill(ctx, x - x_shift, y - y_shift, radius, (0, 0, 0, 0.3))
         cls.draw_circle_fill(ctx, x, y, radius, rgba)
 
     @classmethod
@@ -33,9 +34,9 @@ class Shapes:
                       rotate=0.0):
         # Angle and Shift
         phi = math.acos(shift / (2 * radius))
-        y_shift = shift * math.sin(rotate)
-        x_shift = shift * math.cos(rotate)
+        x_shift, y_shift = shift * math.cos(rotate), shift * math.sin(rotate)
         # Draw crescent
+        x, y = x + x_shift, y + y_shift
         ctx.arc(x, y, radius, rotate + math.pi - phi, rotate + phi - math.pi)
         ctx.arc_negative(x - x_shift, y - y_shift, radius,
                          rotate - phi, rotate + phi)
@@ -43,6 +44,11 @@ class Shapes:
         ctx.close_path()
         ctx.set_source_rgba(*Colour.unpack_rgba(rgba))
         ctx.fill()
+
+    @classmethod
+    def draw_sphere(cls, ctx, x, y, radius, shift, rgba, rotate=0):
+        cls.draw_circle_fill_shade(ctx, x, y, radius, shift, rgba, rotate)
+        cls.draw_crescent(ctx, x, y, radius * 0.94, shift, (0, 0, 0, 0.2), rotate)
 
 if __name__ == '__main__':
     from canvas import Canvas
@@ -54,9 +60,9 @@ if __name__ == '__main__':
     canvas = Canvas(WIDTH, HEIGHT)
     ctx = canvas.ctx
     ims = canvas.ims
-    canvas.background_fill((.2, .2, .2, 0.5))
+    canvas.background_fill((.3, .3, .3))
 
     # Draw shapes
-    Shapes.draw_crescent(ctx, WIDTH / 2, HEIGHT / 2, 200, 50, (1, 0.5, 0.1))
+    Shapes.draw_sphere(ctx, WIDTH / 2, HEIGHT / 2, 200, 50, (1, 0.5, 0.1))
 
     ims.write_to_png('test.png')
