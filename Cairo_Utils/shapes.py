5bd9c96ffb544571a60512e6ea5c8888b3440239
Josh <joshpooley93@gmail.com>
2020-03-07 07:55:39 +0000

    before source seperation

--- Cairo_Utils/shapes.py
+++ Cairo_Utils/shapes.py
@@ -22,10 +22,27 @@ class Shapes:
         ctx.fill()
 
     @classmethod
+    def draw_circle_gradient(cls, ctx, x, y, radius, rgba1, rgba2):
+        grd = cr.RadialGradient(x, y, 0, x, y, radius)
+        grd.add_color_stop_rgba(0, *Colour.unpack_rgba(rgba1))
+        grd.add_color_stop_rgba(1, *Colour.unpack_rgba(rgba2))
+        ctx.set_source(grd)
+        ctx.arc(x, y, radius, 0, 2 * math.pi)
+        ctx.fill()
+
+    @classmethod
     def draw_circle_fill_shade(cls, ctx, x, y,
                                radius, shift, rgba, rotate=0.0):
         x_shift, y_shift = shift * math.cos(rotate), shift * math.sin(rotate)
         cls.draw_circle_fill(ctx, x - x_shift, y - y_shift, radius, (0, 0, 0, 0.3))
+        cls.draw_circle_fill(ctx, x, y, radius, rgba)
+
+    @classmethod
+    def draw_circle_gradient_shade(cls, ctx, x, y,
+                               radius, shift, rgba, rotate=0.0):
+        x_shift, y_shift = shift * math.cos(rotate), shift * math.sin(rotate)
+        cls.draw_circle_gradient(ctx, x - x_shift, y - y_shift,
+                                 radius, (0, 0, 0, 0.6), (0, 0, 0, 0.1))
         cls.draw_circle_fill(ctx, x, y, radius, rgba)
 
     @classmethod
@@ -46,9 +63,14 @@ class Shapes:
         ctx.fill()
 
     @classmethod
-    def draw_sphere(cls, ctx, x, y, radius, shift, rgba, rotate=0):
+    def draw_simple_sphere(cls, ctx, x, y, radius, shift, rgba, rotate=0):
         cls.draw_circle_fill_shade(ctx, x, y, radius, shift, rgba, rotate)
         cls.draw_crescent(ctx, x, y, radius * 0.94, shift, (0, 0, 0, 0.2), rotate)
+
+    @classmethod
+    def draw_gradient_sphere(cls, ctx, x, y, radius, shift, rgba, rotate=0):
+        cls.draw_circle_gradient_shade(ctx, x, y, radius, shift, rgba, rotate)
+        cls.draw_crescent(ctx, x, y, radius * 0.94, shift, (0, 0, 0, 0.1), rotate)
 
 if __name__ == '__main__':
     from canvas import Canvas
@@ -63,6 +85,7 @@ if __name__ == '__main__':
     canvas.background_fill((.3, .3, .3))
 
     # Draw shapes
-    Shapes.draw_sphere(ctx, WIDTH / 2, HEIGHT / 2, 200, 50, (1, 0.5, 0.1))
+    Shapes.draw_gradient_sphere(ctx, WIDTH/2, HEIGHT/2,
+                                      200, 100, (1, 0.2, 0.4))
 
     ims.write_to_png('test.png')
